51537c25d1d991ea7f55f4694772f810357e37e9
Civil Machines Technologies Private Limited <stephin007>
2020-07-14 01:12:35 +0530

    Added heading:octocat:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, {Fragment} from 'react';
 // import axios from 'axios';
 
+import logo from './img/logo.png'
+
 import './App.css';
 
  export default class App extends React.Component {
   render(){
     return(
       <Fragment>
-        
+        <div className="header">
+          <img src={logo} alt="Logo" className="logo-img"/>
+          <h1 className="logo-text">Monetæ</h1>
+        </div>
+
       </Fragment>
     )
   }

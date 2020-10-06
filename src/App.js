3f7e9aa52741263f1dcc7ac1a9f19ff1ce7e3e0c
Jigar Lodaya <jigs1212@users.noreply.github.com>
2020-10-06 14:30:19 +0530

    Added the secondary API (#10)
    * Added the secondary API
    
    * Fixed select style
    
    Co-authored-by: Jigar Lodaya <jigar.l@instamojo.com>

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import logo from './img/logo.png'
 import './App.css';
 
 // API
-const BASE_URL = 'https://api.exchangeratesapi.io/latest'
+const BASE_URL = 'https://api.exchangeratesapi.io/latest';
+const SECONDARY_API = 'https://restcountries.eu/rest/v2/';
 
  export default function App(){
 
@@ -24,12 +25,14 @@ const BASE_URL = 'https://api.exchangeratesapi.io/latest'
   )
 
   const [currencyOptions, setCurrencyOptions] = useState([])
+  const [secondaryData, setSecondaryData] = useState([])
   const [fromCurrency, setFromCurrency] = useState()
   const [toCurrency, setToCurrency] = useState()
   const [exchangeRate, setExchangeRate] = useState()
   const [amount, setAmount] = useState(1)
   const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
-
+  const [data, setData] = useState([]);
+  
 
   let toAmount, fromAmount
   if(amountInFromCurrency){
@@ -41,17 +44,41 @@ const BASE_URL = 'https://api.exchangeratesapi.io/latest'
   }
 
   useEffect(() => {
-        fetch(BASE_URL)
-          .then(res=> res.json())
-          .then(data => {
-            console.log(data)
-            const firstCurrency = Object.keys(data.rates)[11]
-            setCurrencyOptions([data.base, ...Object.keys(data.rates)])
-            setFromCurrency(data.base)
-            setToCurrency(firstCurrency)
-            setExchangeRate(data.rates[firstCurrency])
-          })
-    }, [])
+    fetch(SECONDARY_API + "all?fields=name;capital;currencies;flag;")
+      .then(res => res.json())
+      .then(data => {
+        setSecondaryData(data);
+      })
+    fetch(BASE_URL)
+      .then(res=> res.json())
+      .then(data => {
+        console.log(data)
+        const firstCurrency = Object.keys(data.rates)[11]
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
+        setExchangeRate(data.rates[firstCurrency])
+      })
+    }, []);
+
+    useEffect(() => {
+      if(secondaryData && Object.keys(secondaryData).length > 0) {
+        let listData = [];
+        secondaryData.forEach(element => {
+          if(currencyOptions.indexOf(element.currencies[0].code) > -1) {
+            let data = {
+              country: element.name,
+              countryFlag: element.flag,
+              currencyName: element.currencies[0].name,
+              currencyCode: element.currencies[0].code,
+              currencySymbol: element.currencies[0].symbol,
+            };
+            listData.push(data);
+          }
+        });
+        setData(listData);
+      }
+    }, [secondaryData, currencyOptions])
 
     useEffect(() => {
       if (fromCurrency != null && toCurrency != null) {
@@ -83,26 +110,33 @@ const BASE_URL = 'https://api.exchangeratesapi.io/latest'
             <li><h1 className="logo-text">Monetæ</h1></li>
           </ul>
 
-          <div className="currency-main">
-            <div className="title">
-              <h1>Currency Convertor</h1>
-            </div>
-            <Currency 
-              currencyOptions={currencyOptions}
-              selectedCurrency={fromCurrency}
-              onChangeCurrency={e => setFromCurrency(e.target.value)}
-              onChangeAmount={handleFromAmountChange}
-              amount={fromAmount}
-            />
-            <div className="equals">=</div>
-            <Currency 
-              currencyOptions={currencyOptions}
-              selectedCurrency={toCurrency}
-              onChangeCurrency={e => setToCurrency(e.target.value)}
-              onChangeAmount={handleToAmountChange}
-              amount= {toAmount}
-            />  
-          </div>
+          {data.length > 0 ? 
+            (<div className="currency-main">
+              <div className="title">
+                <h1>Currency Convertor</h1>
+              </div>
+              <Currency 
+                // currencyOptions={currencyOptions}
+                data={data}
+                selectedCurrency={fromCurrency}
+                onChangeCurrency={e => setFromCurrency(e.target.value)}
+                onChangeAmount={handleFromAmountChange}
+                amount={fromAmount}
+              />
+              <div className="equals">=</div>
+              <Currency 
+                // currencyOptions={currencyOptions}
+                data={data}
+                selectedCurrency={toCurrency}
+                onChangeCurrency={e => setToCurrency(e.target.value)}
+                onChangeAmount={handleToAmountChange}
+                amount= {toAmount}
+              />  
+            </div>) : 
+            <>
+            </>
+          }
+
         </div>
         <div className="social-link no-select">
             <a href="https://github.com/stephin007/currencyconvertor" target="_blank" rel = "noopener noreferrer"><i className= "fa fa-github"></i></a>
